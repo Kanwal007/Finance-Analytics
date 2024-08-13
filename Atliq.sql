1e596bcc32a37c307b2b33ac8ab4b278836f5514
Kanwal007 <69477920+Kanwal007@users.noreply.github.com>
2024-08-13 08:40:59 -0400

    Add files via upload

---
+++ Atliq.sql
@@ -0,0 +1,195 @@
+Select * from fact_sales_monthly
+where 
+	customer_code=90002002 and
+	get_fiscal_year(date) = 2021
+order by date asc
+
+### Module: Gross Sales Report: Monthly Product Transactions
+
+-- a. Perform joins to pull product information
+	SELECT s.date, s.product_code, p.product, p.variant, s.sold_quantity 
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+        ON s.product_code=p.product_code
+	WHERE 
+            customer_code=90002002 AND 
+    	    get_fiscal_year(date)=2021     
+	LIMIT 1000000;
+    -- b. Performing join with 'fact_gross_price' table with the above query and generating required fields
+	SELECT 
+    	    s.date, 
+            s.product_code, 
+            p.product, 
+            p.variant, 
+            s.sold_quantity, 
+            g.gross_price,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+            ON g.fiscal_year=get_fiscal_year(s.date)
+    	AND g.product_code=s.product_code
+	WHERE 
+    	    customer_code=90002002 AND 
+            get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
+    ### Module: Gross Sales Report: Total Sales Amount
+
+-- Generate monthly gross sales report for Croma India for all the years
+	SELECT 
+            s.date, 
+    	    SUM(ROUND(s.sold_quantity*g.gross_price,2)) as monthly_sales
+	FROM fact_sales_monthly s
+	JOIN fact_gross_price g
+        ON g.fiscal_year=get_fiscal_year(s.date) AND g.product_code=s.product_code
+	WHERE 
+             customer_code=90002002
+	GROUP BY date;
+    
+    ### Module: Stored Procedure: Market Badge
+
+--  Write a stored proc that can retrieve market badge. i.e. if total sold quantity > 5 million that market is considered "Gold" else "Silver"
+	CREATE PROCEDURE `get_market_badge`(
+        	IN in_market VARCHAR(45),
+        	IN in_fiscal_year YEAR,
+        	OUT out_level VARCHAR(45)
+	)
+	BEGIN
+             DECLARE qty INT DEFAULT 0;
+    
+    	     # Default market is India
+    	     IF in_market = "" THEN
+                  SET in_market="India";
+             END IF;
+    
+    	     # Retrieve total sold quantity for a given market in a given year
+             SELECT 
+                  SUM(s.sold_quantity) INTO qty
+             FROM fact_sales_monthly s
+             JOIN dim_customer c
+             ON s.customer_code=c.customer_code
+             WHERE 
+                  get_fiscal_year(s.date)=in_fiscal_year AND
+                  c.market=in_market;
+        
+             # Determine Gold vs Silver status
+             IF qty > 5000000 THEN
+                  SET out_level = 'Gold';
+             ELSE
+                  SET out_level = 'Silver';
+             END IF;
+	END
+### Module: Problem Statement and Pre-Invoice Discount Report    
+    SELECT 
+    	   s.date, 
+           s.product_code, 
+           p.product, 
+	   p.variant, 
+           s.sold_quantity, 
+           g.gross_price as gross_price_per_item,
+           ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+           pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    	    ON g.fiscal_year=get_fiscal_year(s.date)
+    	    AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+            ON pre.customer_code = s.customer_code AND
+            pre.fiscal_year=get_fiscal_year(s.date)
+	WHERE 
+    	    get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
+    
+   ### Module: Top Markets and Customers 
+
+-- Get top 5 market by net sales in fiscal year 2021
+	SELECT 
+    	    market, 
+            round(sum(net_sales)/1000000,2) as net_sales_mln
+	FROM gdb0041.net_sales
+	where fiscal_year=2021
+	group by market
+	order by net_sales_mln desc
+	limit 5 
+
+### Module: Window Functions: Using it In a Task
+
+-- find out customer wise net sales percentage contribution 
+	with cte1 as (
+		select 
+                    customer, 
+                    round(sum(net_sales)/1000000,2) as net_sales_mln
+        	from net_sales s
+        	join dim_customer c
+                    on s.customer_code=c.customer_code
+        	where s.fiscal_year=2021
+        	group by customer)
+	select 
+            *,
+            net_sales_mln*100/sum(net_sales_mln) over() as pct_net_sales
+	from cte1
+	order by net_sales_mln desc
+    
+    -- Find out top 3 products from each division by total quantity sold in a given year
+	with cte1 as 
+		(select
+                     p.division,
+                     p.product,
+                     sum(sold_quantity) as total_qty
+                from fact_sales_monthly s
+                join dim_product p
+                      on p.product_code=s.product_code
+                where fiscal_year=2021
+                group by p.product),
+           cte2 as 
+	        (select 
+                     *,
+                     dense_rank() over (partition by division order by total_qty desc) as drnk
+                from cte1)
+	select * from cte2 where drnk<=3
+    
+    ### Module: Create a Helper Table
+
+-- Create fact_act_est table
+	drop table if exists fact_act_est;
+
+	create table fact_act_est
+	(
+        	select 
+                    s.date as date,
+                    s.fiscal_year as fiscal_year,
+                    s.product_code as product_code,
+                    s.customer_code as customer_code,
+                    s.sold_quantity as sold_quantity,
+                    f.forecast_quantity as forecast_quantity
+        	from 
+                    fact_sales_monthly s
+        	left join fact_forecast_monthly f 
+        	using (date, customer_code, product_code)
+	)
+	union
+	(
+        	select 
+                    f.date as date,
+                    f.fiscal_year as fiscal_year,
+                    f.product_code as product_code,
+                    f.customer_code as customer_code,
+                    s.sold_quantity as sold_quantity,
+                    f.forecast_quantity as forecast_quantity
+        	from 
+		    fact_forecast_monthly  f
+        	left join fact_sales_monthly s 
+        	using (date, customer_code, product_code)
+	);
+   SET SQL_SAFE_UPDATES = 0;  
+    update fact_act_est
+	set sold_quantity = 0
+	where sold_quantity is null;
+    
+    update fact_act_est
+	set forecast_quantity = 0
+	where forecast_quantity is null;
+    
